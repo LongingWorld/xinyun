c6bc79cc7b2a4f90c08c1c2d2bb977407b8bc81c
LongingWorld <shadowzzj@126.com>
2018-07-13 20:25:35 +0800

    Post test

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"xinyun/handler"
 
@@ -15,8 +16,15 @@ import (
 */
 
 func main() {
+	gin.SetMode(gin.DebugMode) //全局设置环境，此为开发环境，线上环境为gin.ReleaseMode
 	router := gin.Default()
+	//添加中间件
+	router.Use(Middleware)
 	router.POST("/first", handler.PostHandler)
 
 	http.ListenAndServe(":8000", router)
+}
+
+func Middleware(c *gin.Context) {
+	fmt.Println("this is a middleware!")
 }
